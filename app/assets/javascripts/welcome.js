3f3dca441baab4fba2357f5b858c38d04923017f
Mike Labita <mlabita37@gmail.com>
2016-01-28 11:48:56 -0500

    Adds Angular and get/post methods to welcome controller

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,2 +1,19 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+var app = angular.module('Fanster', []);
+
+app.controller('FansController', ['$scope', '$http', function($scope, $http){
+
+  $http.get('/api/fans/').then(function(response){
+    var data = response.data;
+    $scope.fans = data.fans;
+  });
+
+  $scope.newFan = {};
+  $scope.createTroll = function(){
+    $http.post('/api/fans', {fans: $scope.newFan}).then(function(response){
+      var data = response.data;
+      $scope.fans.push( data );
+    });
+  };
+
+
+}]);
