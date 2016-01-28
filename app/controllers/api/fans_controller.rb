f17645f4fb5a3cc3a5b8ae2cc3f4d85b2181efee
Mike Labita <mlabita37@gmail.com>
2016-01-28 11:41:36 -0500

    Adds methods to FansController

--- app/controllers/api/fans_controller.rb
+++ app/controllers/api/fans_controller.rb
@@ -2,4 +2,16 @@ class Api::FansController < ApplicationController
   def index
     fans = Fan.all
     render json: {fans: fans}
+  end
+
+  def create
+    new_fan = Fan.create( fan_params )
+    render json: new_fan
+  end
+
+  private
+  def fan_params
+    params.require(:fan).permit(:name, :age, :city, :state, :country, :email)
+  end
+
 end
