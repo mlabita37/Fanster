b1a4a6f72c79f7b7250a49e968b2e96d81b4fb90
Mike Labita <mlabita37@gmail.com>
2016-01-28 11:58:19 -0500

    Adds test data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Fan.create({
+  name:'Mike Labita',
+  age: 31,
+  city: 'Brooklyn',
+  state: 'NY',
+  country: 'USA',
+  email: 'mlabita37@gmail.com'
+})
